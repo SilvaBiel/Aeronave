680441277322acbcd4a610dc3a903b20e51a8e26
bielusantos <bielusantos@ucl.br>
2020-10-13 23:34:03 +0000

    Aviao criado

--- main.cs
+++ main.cs
@@ -2,6 +2,7 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
+    Aeronave Aviao = new Aeronave ("Gripen", "Marcos Rocha", "Guarulhos", 5000, 3000, 150, 3, 900, 4000, 123)
     Console.WriteLine ("Hello World");
   }
 }
